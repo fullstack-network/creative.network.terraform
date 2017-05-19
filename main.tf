a20dd9c3751f1736e0e7a9f7b928c245adf980a1
KensoDev <avi@kensodev.com>
2017-05-18 22:00:57 -0700

    Empty function and removing the S3 bucket for function code

--- main.tf
+++ main.tf
@@ -15,20 +15,36 @@ resource "aws_s3_bucket" "images" {
   }
 }
 
-resource "aws_s3_bucket" "lambda_functions" {
-  bucket = "creative-network-lambda"
-
-  versioning {
-    enabled = false
-  }
-
-  tags {
-    Name = "creative-network-lambda"
-    Environment = "dev"
-  }
-}
-
 resource "aws_api_gateway_rest_api" "resizer" {
   name        = "image-resizer"
   description = "Creative network image resizer"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "empty-function.zip"
+  source_code_hash = "${base64sha256(file("empty-function.zip"))}"
+  function_name    = "image-resizer"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "index.handler"
+  runtime          = "nodejs6.10"
 }
